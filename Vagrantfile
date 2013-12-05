5a5ef4527c910cdebd5cae546d76465b5832474b
Leonardo Thibes <eu@leonardothibes.com>
2013-12-05 10:38:17 -0200

    alterando local do manifest do puppet

--- Vagrantfile
+++ Vagrantfile
@@ -58,8 +58,9 @@ Vagrant::Config.run do |config|
   # # }
   #
   config.vm.provision :puppet do |puppet|
-    puppet.manifests_path = "puppet"
-    puppet.manifest_file  = "default.pp"
+    puppet.manifests_path = "puppet/manifests"
+    puppet.manifest_file  = "init.pp"
+    puppet.module_path    = "puppet/modules"
   end
 
   # Enable provisioning with chef solo, specifying a cookbooks path, roles
---
+++ puppet/manifests/init.pp
@@ -0,0 +1,63 @@
+#class env {
+#	file {"/home/vagrant/blog":
+#		ensure => link,
+#		target => "/vagrant",
+#	}
+#}
+
+#class vim {
+#    
+#    $vim     = "vim"
+#	$vimpath = "/etc/vim"
+#	
+#	package {[ "$vim" , "$vim-puppet" ]: ensure => present}
+#	exec {"vim-addons install puppet": path => "/usr/bin"}
+#	exec {"update-alternatives --set editor /usr/bin/vim.basic":
+#		path    => "/usr/bin:/usr/sbin:/bin",
+#		unless  => "test /etc/alternatives/editor -ef /usr/bin/vim.basic",
+#		require => Package["$vim"],
+#	}
+#	file {"$vimpath/vimrc.local":
+#		ensure  => present,
+#		source  => "/tmp/vagrant-puppet/manifests/files/vim/vimrc.local",
+#		owner   => root,
+#		group   => root,
+#		mode    => 0644,
+#		require => Package["$vim"],
+#	}
+#}
+
+#class jekyll {
+#    $packages = [
+#		'wget',
+#		'curl',
+#		'tar',
+#		'zip',
+#		'unzip',
+#		'bzip2',
+#		'git',
+#		'build-essential',
+#	]
+#	package {$packages: ensure  => installed}
+#	package {"tzdata" : ensure  => latest   }
+#	
+#	exec {"gem install jekyll":
+#		path      => ["/opt/vagrant_ruby/bin", "/usr/bin"],
+#		require   => Package[$packages],
+#		logoutput => true,
+#	}
+#
+#	exec {"gem install RedCloth":
+#		path      => ["/opt/vagrant_ruby/bin", "/usr/bin"],
+#		require   => Package[$packages],
+#		logoutput => true,
+#	}
+#}
+
+#stage {'preinstall': before => Stage['main']}
+#class apt_get_update { exec {'apt-get -y update': path => "/usr/bin" } }
+#class {'apt_get_update': stage => preinstall}
+
+#include env
+#include vim
+#include jekyll
