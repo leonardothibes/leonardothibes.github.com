a9ba6e245a3d4a0ec6e7ce4e3c6f115c504546d7
Leonardo Thibes <eu@leonardothibes.com>
2014-03-25 13:15:32 -0300

    reorganizando receita do puppet para usar os modulos "leonardothibes-env","leonardothibes-vim" e "leonardothibes-jekyll" #51

--- .puppet/manifests/init.pp
+++ .puppet/manifests/init.pp
@@ -1,7 +1,9 @@
-stage {'preinstall': before => Stage['main']}
-class apt_get_update { exec {'apt-get -y update': path => "/usr/bin" } }
-class {'apt_get_update': stage => preinstall}
-
-include env
-include vim
+class {'env':
+	utils        => ['git','make'],
+	link_in_home => ['blog=/vagrant'],
+}
+class {'vim':
+	tabstop  => 4,
+	opt_misc => ['number','nowrap'],
+}
 include jekyll
